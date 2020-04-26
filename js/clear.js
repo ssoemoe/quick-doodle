606292da14383d859bf2c30984a35a23a1ad5326
Shane Moe <ssoemoe@terpmail.umd.edu>
2020-04-26 13:47:46 -0400

    refactor: clear feature modified

--- js/clear.js
+++ js/clear.js
@@ -3,13 +3,10 @@ clearBtn.addEventListener('click', function (event) {
     context.clearRect(0, 0, canvas.width, canvas.height);
     actionHistory.push(recent);
     recent = [];
-    if (actionHistory.length > 0 && actionHistory[actionHistory.length - 1][0].action !== "clear") {
-        console.log(actionHistory[actionHistory.length - 1][0].action);
-        actionHistory.push([
-            {
-                action: "clear"
-            }
-        ]);
-    }
+    actionHistory.push([
+        {
+            action: "clear"
+        }
+    ]);
     currentViewIndex = actionHistory.length - 1;
 });
